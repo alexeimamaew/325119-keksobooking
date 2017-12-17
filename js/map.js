b1bee358b61893eac4a306883ab4c6755d2ab650
Алексей <alexeimamaew@yandex.ru>
2017-12-17 18:39:24 +0300

    m4t2

--- js/map.js
+++ js/map.js
@@ -22,6 +22,19 @@
   var popup = null;
   var apartments = null;
 
+  // переменные для работы с формами
+  var offerForm = document.querySelector('.notice__form');
+  var offerTitle = offerForm.querySelector('#title');
+  var offerAddress = offerForm.querySelector('#address');
+  var offerType = offerForm.querySelector('#type');
+  var offerPrice = offerForm.querySelector('#price');
+  var offerRoom = offerForm.querySelector('#room_number');
+  var offerCapacity = offerForm.querySelector('#capacity');
+  var offerTimeIn = offerForm.querySelector('#timein');
+  var offerTimeOut = offerForm.querySelector('#timeout');
+  // ассоциативный массив  стоимости и типа жилья
+  var offerMinPrice = {'bungalo': 0, 'flat': 1000, 'house': 5000, 'palace': 10000};
+
   // функция получает удобства в объявлениях
   function getFeatures(features) {
     var listLength = features.length;
@@ -222,6 +235,74 @@
       addPinAction();
     });
   }
+
+
+  // функции валидации
+  // функция валидация input[type = 'text']
+  function inputValidate(min, max, numeric) {
+    return function (evt) {
+      var target = evt.target;
+      var inputValue = numeric ? parseFloat(target.value) : target.value.length;
+      var messageText = '';
+      if (min > 0 && inputValue <= min) {
+        messageText = numeric ? 'Значение в поле должно быть больше ' + min : 'Минимальная длина содержимого поля - ' + min;
+      } else if (max > min && inputValue >= max) {
+        messageText = numeric ? 'Значение в поле должно быть меньше ' + max : 'Максимальная длина содержимого поля ' + max + ' символов';
+      }
+      target.setCustomValidity(messageText);
+    };
+  }
+
+  // события ввода
+  offerTitle.addEventListener('input', inputValidate(30, 100, false));
+  offerAddress.addEventListener('input', inputValidate(0, 0, false));
+  offerPrice.addEventListener('input', inputValidate(offerPrice.min, offerPrice.max, true));
+
+  // синхронизация  времени въезда и выезда
+  function timeAutoCorrect(source, dest) {
+    dest.selectedIndex = source.selectedIndex;
+  }
+
+  // синхронизация цены в зависимости от типа объекта
+  function priceAutoCorrect(source, dest) {
+    var minPriceValue = offerMinPrice[source.value];
+    dest.min = minPriceValue;
+    if (dest.value < minPriceValue) {
+      dest.value = minPriceValue;
+    }
+  }
+
+  // функция валидации поля количества комнат
+  function roomInputValidate(source, dest) {
+    var capacityLength = dest.options.length;
+    var roomValue = parseInt(source.value, 10);
+    var currentCapacity = parseInt(dest.value, 10);
+    for (var i = 0; i < capacityLength; i++) {
+      var capacityVal = parseInt(dest.options[i].value, 10);
+      dest.options[i].disabled = (capacityVal > roomValue) || (roomValue !== 100 && capacityVal === 0) || (capacityVal > 0 && roomValue === 100);
+      if (!dest.options[i].disabled && (roomValue < currentCapacity || capacityVal === 0 || currentCapacity === 0)) {
+        dest.options[i].selected = true;
+      }
+    }
+  }
+
+  // функция синхронизирует поля
+  function fieldsAutoCorrect(sourceElement, destElement, synchronizeFunction) {
+    if (synchronizeFunction && typeof synchronizeFunction === 'function') {
+      sourceElement.addEventListener('change', function () {
+        synchronizeFunction(sourceElement, destElement);
+      });
+    }
+  }
+
+  // объявление функций формы
+  fieldsAutoCorrect(offerTimeOut, offerTimeIn, timeAutoCorrect);
+  fieldsAutoCorrect(offerTimeIn, offerTimeOut, timeAutoCorrect);
+  fieldsAutoCorrect(offerType, offerPrice, priceAutoCorrect);
+  fieldsAutoCorrect(offerRoom, offerCapacity, roomInputValidate);
+  onload = function () {
+    roomInputValidate(offerRoom, offerCapacity);
+  };
 
   cardActivation(); // раздизабливание карты
   apartments = createApartments(8, OFFER_TITLES, OFFER_TYPE, OFFFER_CHECKIN_CHECKOUT, OFFER_FEATURES); // создание карточек объявлений
