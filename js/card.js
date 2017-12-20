dcb25630f66077b8e91db131c429437b418d1250
Алексей <alexeimamaew@yandex.ru>
2017-12-21 00:20:22 +0300

    коммит #2

--- js/card.js
+++ js/card.js
@@ -1,93 +1,67 @@
 'use strict';
 
-(function () {
+window.card = (function() {
+  var ENTER_KEYCODE = 13;
+  var offerDialog = document.querySelector('.popup');
+  var closeDialogBtn = offerDialog.querySelector('.popup__close');
+  var dialogTemplateCopy = document.querySelector('template').content;
+  var TYPE_DESCS = ['Квартира', 'Бунгало', 'Дом'];
 
-   var ESC_KEYCODE = 27;
-   var ENTER_KEYCODE = 13;
-   var popup = null;
-   var translate = {
-     flat: 'Квартира',
-     bungalo: 'Бунгало',
-     house: 'Дом'
-   };
-  var popupCloseOpen = false;
-
-
-   // функция получает удобства в объявлениях
-  function getFeatures(features) {
-    var listLength = features.length;
-    var featureString = '';
-    for (var i = 0; i < listLength; i++) {
-      featureString += '<li class="feature feature--' + OFFER_FEATURES[i] + '" ></li>';
+  // функция переводит типы жилья в русскоязычный формат
+  function getRusType(type) {
+    switch (type) {
+      case 'flat':
+        var humanType = TYPE_DESCS[0];
+        break;
+      case 'bungalo':
+        humanType = TYPE_DESCS[1];
+        break;
+      case 'house':
+        humanType = TYPE_DESCS[2];
+        break;
     }
-    return featureString;
+    return humanType;
   }
 
-  // функция добавления слушателя в popup
-  function addPopupListener() {
-    var popupClose = map.querySelector('.popup__close');
-    popupClose.addEventListener('click', closeDialog);
-    map.addEventListener('keydown', onPopupEscPress);
-    popupClose.addEventListener('keydown', onPopupEnterPress);
+  function hideCard() {
+    offerDialog.classList.add('hidden');
   }
 
-  // функция закрытия popup
-  function closeDialog() {
-    popup.classList.add('hidden');
-    prefClickAtButton.classList.remove('map__pin--active');
-  }
+  // закрытие popup мышью
+  closeDialogBtn.addEventListener('click', hideCard);
 
-  // функция закрытия popup по кнопке ESC
-  function onPopupEscPress(evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      closeDialog();
-    }
-  }
-
-  // функция закрытия popup по кнопке Enter
-  function onPopupEnterPress(evt) {
+  // закрытие при нажатии enter, когда крестик в фокусе
+  closeDialogBtn.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
-      closeDialog();
+      hideCard();
     }
+  });
+var offerDialog = document.querySelector('.popup');
+  function renderOffer(generatedOffer) {
+    var dialogDesc = document.querySelector('.map__card');
+    var dialogTemplate = dialogTemplateCopy.cloneNode(true);
+    ar.replaceChild(dialogTemplate, dialogDesc);
+    offerDialog.querySelector('h3').textContent = generatedOffer.offer.title;
+    offerDialog.querySelector('small').textContent = generatedOffer.offer.address;
+    offerDialog.querySelector('.popup__price').textContent = generatedOffer.offer.price + '&#x20bd;/ночь';
+    offerDialog.querySelector('h4').textContent = getRusType(generatedOffer.offer.type);
+    offerDialog.querySelector('h4+p').textContent = 'Для ' + generatedOffer.offer.guests + ' гостей в ' + generatedOffer.offer.rooms + ' комнатах';
+    offerDialog.querySelector('h4+p+p').textContent = 'Заезд после ' + generatedOffer.offer.checkin + ', выезд до ' + generatedOffer.offer.checkout;
+    generatedOffer.offer.features.forEach(function (renderElem) {
+    offerDialog.querySelector('.popup__features').insertAdjacentHTML('afterbegin', '<li class="feature feature--' + renderElem + '">');
+    });
+    offerDialog.querySelector('ul+p').textContent = generatedOffer.offer.description;
+    offerDialog.querySelector('.popup__avatar').querySelector('img').src = generatedOffer.author.avatar;
   }
 
-  window.card = {
-  // функция показывает карточки объявлений с заполненными данными
-  renderApartmentContent: function (obj) {
-    var templateArticle = template.content.querySelector('.map__card');
-    var article = null;
-    var mapFilters = map.querySelector('.map__filters-container');
+  return {
 
-    if (popupCloseOpen === true) {
-      article = map.querySelector('.popup');
-      popup = article;
-    } else {
-      article = templateArticle.cloneNode(true);
-      popup = map.querySelector('.popup');
-    }
-    article.querySelector('h3').textContent = obj.offer.title;
-    article.querySelector('small').textContent = obj.offer.address;
-    article.querySelector('.popup__price').innerHTML = obj.offer.price + ' &#x20bd;/ночь';
-    article.querySelector('h4').textContent = translate[obj.offer.type];
-    article.querySelector('h4+p').textContent = obj.offer.rooms + ' для ' + obj.offer.guests + ' гостей';
-    article.querySelector('h4+p+p').textContent = 'Заезд после ' + obj.offer.checkin + ' выезд до ' + obj.offer.checkout;
-    article.querySelector('.popup__features').innerHTML = getFeatures(obj.offer.features);
-    article.querySelector('ul+p').textContent = obj.offer.description;
-    article.querySelector('.popup__avatar').setAttribute('src', obj.author.avatar);
-    map.insertBefore(article, mapFilters); // вставляет элемент article перед mapFilters
+    show: function showCard(data) {
+      offerDialog.classList.remove('hidden');
+      renderOffer(data);
+    },
 
-    addPopupListener(); // добавление слушателя в popup
-    popupCloseOpen = true;
-  }
-  }
+    hide: hideCard
 
-  function shomAppartmentPopup(string) {
-    for (var i = 0; i < apartments.length; i++) {
-      if (apartments[i].author.avatar === string) {
-        renderApartmentContent(apartments[i]);
-        popup = map.querySelector('.popup');
-      }
-    }
-  }
-
-}());
+  };
+})();
